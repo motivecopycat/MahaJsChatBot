600aca6288529dc89c9f8917bed14def682ebcc4
motivecopycat <motivecopycat@gmail.com>
2026-02-26 09:52:04 +0530

    Update firebase.js

--- services/firebase.js
+++ services/firebase.js
@@ -1,16 +1,12 @@
 import admin from "firebase-admin";
-import dotenv from "dotenv";
-
-dotenv.config();
-
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 if (!admin.apps.length) {
+  const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   });
 }
 
 const db = admin.firestore();
-
 export default db;
