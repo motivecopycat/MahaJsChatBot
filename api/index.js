65848c7f06ad7063d1366bbf0ab0b219e8581f2d
motivecopycat <motivecopycat@gmail.com>
2026-02-28 03:24:37 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -39,7 +39,7 @@ export default async function handler(req, res) {
         username,
         name,
         joinDate: new Date(),
-        status: "0",
+        status: "active",
         step: ""
       });
 
